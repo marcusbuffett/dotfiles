bfc0f36cd2b55d43b6191804e9074bf3586ca9ec
Marcus Buffett <marcusbuffett@me.com>
2015-06-28 17:20:18 -0400

    Make directory before downloading antigen

--- makelinks.sh
+++ makelinks.sh
@@ -8,6 +8,7 @@ done
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+mkdir ~/antigen
 curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > ~/antigen/antigen.zsh
 
 vim +PlugInstall
